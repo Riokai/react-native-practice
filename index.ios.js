7d50e2908a5cbf6b58fb80a79d6ef276767b3de1
Kai <zengkai2009@foxmail.com>
2016-12-07 18:04:59 +0800

    use PopupDialog

--- index.ios.js
+++ index.ios.js
@@ -7,40 +7,7 @@ import {
   AlertIOS
 } from 'react-native';
 import NetEase from './src/page/NetEase'
+import App from './src/'
 
-export default class demo extends Component {
-  componentDidMount() {
-    AlertIOS.prompt('info', 'react native', [
-      {
-        text: 'Cancel',
-        onPress() {
-          console.log('cancel');
-        }
-      },
-      {
-        text: 'OK',
-        onPress(e) {
-          console.log('sure', e);
-        }
-      }
-    ])
-  }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        {/* <NetEase /> */}
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 25,
-    backgroundColor: '#fff',
-  }
-});
-
-AppRegistry.registerComponent('demo', () => demo);
+AppRegistry.registerComponent('demo', () => App)
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
+import PopupDialog, { ScaleAnimation, DialogTitle } from 'react-native-popup-dialog'
 
 export default class Main extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
+        <Text style={styles.welcome} onPress={() => {
+          this.popupDialog.openDialog()
+        }}>
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
@@ -15,8 +18,21 @@ export default class Main extends Component {
           Double tap R on your keyboard to reload,{'\n'}
           Shake or press menu button for dev menu
         </Text>
+        <PopupDialog
+          ref={(popupDialog) => { this.popupDialog = popupDialog }}
+          dialogTitle={<DialogTitle title="Dialog Title" />}
+          dialogAnimation = { new ScaleAnimation() }
+        >
+          <View>
+            <TextInput style={{
+              width: 200,
+              paddingLeft: 30,
+              height: 30
+            }} />
+          </View>
+        </PopupDialog>
       </View>
-    );
+    )
   }
 }
 
